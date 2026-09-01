1cbe37c4fc37d008b6062e09c3ad9b8724eab250
Régulo Luna <90281028+Regulo-Luna@users.noreply.github.com>
2026-09-01 08:26:48 -0300

    Delete src directory

--- src/main/java/com/uade/tpejemplo/repository/MetaCobranzaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.uade.tpejemplo.repository;
-
-import com.uade.tpejemplo.model.MetaCobranza;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MetaCobranzaRepository extends JpaRepository<MetaCobranza, Long> {
-}
--- src/main/java/com/uade/tpejemplo/service/DashboardService.java
+++
@@ -1,7 +0,0 @@
-package com.uade.tpejemplo.service;
-
-import com.uade.tpejemplo.dto.response.DashboardStatsResponseDTO;
-
-public interface DashboardService {
-    DashboardStatsResponseDTO obtenerEstadisticasGenerales();
-}
